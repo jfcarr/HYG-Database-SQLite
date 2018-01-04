958e3ef3cb7d4a7aa0060405023cb004137df004
jfcarr <jfcarr@gmail.com>
2018-01-03 20:15:41 -0500

    New DSO table.

---
+++ schema-dso.sql
@@ -0,0 +1,23 @@
+CREATE TABLE dso
+(
+	"RightAscension" REAL,
+	"Declination" REAL,
+	"NGCType" TEXT,
+	"Constellation" TEXT,
+	"Magnitude" REAL,
+	"CommonName" TEXT,
+	"RARadians" REAL,
+	"DecRadians" REAL,
+	"id" INTEGER,
+	"SemiMajorAxes" REAL,
+	"SemiMinorAxes" REAL,
+	"PositionAngle" REAL,
+	"dso_source" INTEGER,
+	"PrimaryNumberID" INTEGER,
+	"PrimaryCatalogName" TEXT,
+	"AdditionalNumberID" INTEGER,
+	"AdditionalCatalogName" TEXT,
+	"DuplicateNumberID" INTEGER,
+	"DuplicateCatalogName" TEXT,
+	"DisplayMagnitude" REAL
+);
